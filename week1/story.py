1b65c95cc8636df21085f4339fb99a15fab134e3
clopezmar4-ux <clopezmar4@gmail.com>
2026-08-13 15:40:41 +0000

    MEJOR VENDEME Y COMPRATE UN CONEJO!?

--- week1/story.py
+++ week1/story.py
@@ -13,16 +13,26 @@ def main():
         # #Ending
         # print("Hello", end=" ")
         # print(planet)
-    name = input("What is your name? ")
-    color = input("Tell me a color: ")
-    adjective = input("Give me an adjective: ")
-    goal = input("A goal you would like to achive: ")
+    name = input("What is your name? ").strip().title()
+    color = input("Tell me a color: ").strip().lower()
+    adjective = input("Give me an adjective: ").strip().lower()
+    goal = input("A goal you would like to achive: ").strip().lower()
 
     print(f"Hello, {name}!", end="\n\n")
 
     print("This is your story:")
 
     print(f"A dawn the sky turned {color}, and the air felt {adjective}. I decided today i will finally {goal}.")
+
+    print("....................................................")
+
+    print(f"Hello, {name}!", end="\n\n")
+
+    print("This is your story:")
+
+    print(f"A dawn the sky turned {color}, and the air felt {adjective}. I decided today i will finally {goal}.".upper())
+
+
 
 if __name__== "__main__":
         main()
