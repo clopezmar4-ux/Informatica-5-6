975066eb0c546fbfb2a57ae57a6452566902520d
clopezmar4-ux <clopezmar4@gmail.com>
2026-08-17 15:00:29 +0000

    y se marchooo--y a su barco le llamo-- LIBERTAD

---
+++ week2/rectangle.py
@@ -0,0 +1,12 @@
+def main():
+    w= int(input("Enter width: "))
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    p= 2*(w+5)
+    print(" Perimeter:",p)
+
+if __name__=="__main__":
+    main()
