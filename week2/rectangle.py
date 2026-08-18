0ea7b5efdff0cdc6152ae8c95f35d56179fbb7a2
clopezmar4-ux <clopezmar4@gmail.com>
2026-08-18 19:46:10 +0000

    TENGO SUEÑO!!!!!!!

--- week2/rectangle.py
+++ week2/rectangle.py
@@ -1,12 +1,18 @@
 def main():
     w= int(input("Enter width: "))
-    print("O"*w)
-    print("O"*w)
-    print("O"*w)
-    print("O"*w)
-    print("O"*w)
     p= 2*(w+5)
+    a= 5*w
+    d= (5**2+w**2)**1/2
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+    print("O"*w)
+
     print(" Perimeter:",p)
+    print(" Area:",a)
+    print(" Diagonal:",d)
+
 
 if __name__=="__main__":
     main()
