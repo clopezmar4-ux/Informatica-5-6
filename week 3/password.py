f6ad6d8f0263ee98b7addacc1d465b48f9da0612
clopezmar4-ux <clopezmar4@gmail.com>
2026-08-24 15:02:05 +0000

    tengo hambreeeee

---
+++ week 3/password.py
@@ -0,0 +1,12 @@
+
+def main():
+    
+    password = 202920
+    attempt = int(input("Enter the password: "))
+    if  attempt == password:
+        print("Correct password")
+
+    print("See you next time")
+
+if __name__=="__main__":
+    main()
