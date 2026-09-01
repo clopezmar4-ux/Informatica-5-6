8c13bcdf7cf0135f5ee073a142fa6cc8466c1f3f
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-01 20:19:26 +0000

    I believe i can fly!!!!

---
+++ week4/coinflip.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+
+    person = int(input("If you want heads put 1 but if you want tails put 2: "))
+
+    coin = random.randint(1,2)
+    if coin == 1:
+        print("Heads")
+    elif coin == 2:
+        print("Tails")
+    if person == coin:
+        print("winner")
+    elif person != coin:
+        print("Loser")
+    else:
+        print("Wrong information")
+
+
+
+
+if __name__=="__main__":
+    main()
