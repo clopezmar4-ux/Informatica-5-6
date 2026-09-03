f61de6768a3c074788322e4f6864771e998bc591
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-03 15:50:13 +0000

    vamos a la obra en una sola maniobra

---
+++ week4/guess-number.py
@@ -0,0 +1,58 @@
+import random
+
+def main():
+    easy = random.randint(1,20)
+    medium = random.randint(1,100)
+    harder = random.randint(1,1000)
+    number = int()
+    attempt = 3
+
+    user = input("Hello! What is your name? ").title()
+    level = input("which difficulty level you want to play, easy, medium or harder? ")
+    if level == "easy":
+        print(f"Well, {user}, I am thinking of a number between 1 and 20")
+        while attempt > 0:
+            while number != easy:
+                number = int(input("Take a guess: "))
+                if number > easy:
+                    print("Your guess is too high")
+                    attempt -= 1
+                    print("Attempts left:", attempt)
+                elif number < easy:
+                    print("Your guess is too low")
+                    attempt -= 1
+                    print("Attempts left:", attempt)
+                elif number == easy:
+                    print(f"Good job, {user}! You guessed my number!")
+                    break
+                else:
+                    attempt == 0
+                    print("Sorry you lose")
+
+    elif level == medium:
+        print(f"Well, {user}, I am thinking of a number between 1 and 100")
+        while number != medium:
+            number = int(input("Take a guess: "))
+            if number > medium:
+                print("Your guess is too high")
+            elif number < medium:
+                print("Your guess is too low")
+            else:
+                print(f"Good job, {user}! You guessed my number!")
+                break
+    elif level == harder:
+        print(f"Well, {user}, I am thinking of a number between 1 and 1000")
+        while number != harder:
+            number = int(input("Take a guess: "))
+            if number > harder:
+                print("Your guess is too high")
+            elif number < harder:
+                print("Your guess is too low")
+            else:
+                print(f"Good job, {user}! You guessed my number!")
+                break
+
+
+
+if __name__=="__main__":
+    main()
