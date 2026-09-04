5a2da388cbae025f6eac56265c1e1494be767b6c
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-04 22:07:05 +0000

    por fin termine!!!!!

--- week4/guess-number.py
+++ week4/guess-number.py
@@ -4,7 +4,6 @@ def main():
     easy = random.randint(1,20)
     medium = random.randint(1,100)
     harder = random.randint(1,1000)
-    number = int()
     attempt = 3
 
     user = input("Hello! What is your name? ").title()
@@ -12,45 +11,59 @@ def main():
     if level == "easy":
         print(f"Well, {user}, I am thinking of a number between 1 and 20")
         while attempt > 0:
-            while number != easy:
-                number = int(input("Take a guess: "))
-                if number > easy:
-                    print("Your guess is too high")
-                    attempt -= 1
-                    print("Attempts left:", attempt)
-                elif number < easy:
-                    print("Your guess is too low")
-                    attempt -= 1
-                    print("Attempts left:", attempt)
-                elif number == easy:
-                    print(f"Good job, {user}! You guessed my number!")
-                else:
-                    attempt == 0
-                    print("Sorry you lose")
-                    break
+            number = int(input("Take a guess: "))
+            if number > easy:
+                print("Your guess is too high")
+                attempt -= 1
+                print("Attempts left:", attempt)
+            elif number < easy:
+                print("Your guess is too low")
+                attempt -= 1
+                print("Attempts left:", attempt)
+            elif number == easy:
+                print(f"Good job, {user}! You guessed my number!")
+                break
+            else:
+                attempt = 0
+                print("Sorry you lose")
 
-    elif level == medium:
+
+    elif level == "medium":
         print(f"Well, {user}, I am thinking of a number between 1 and 100")
-        while number != medium:
+        while attempt > 0:
             number = int(input("Take a guess: "))
             if number > medium:
                 print("Your guess is too high")
+                attempt -= 1
+                print("Attempts left:", attempt)
             elif number < medium:
                 print("Your guess is too low")
-            else:
+                attempt -= 1
+                print("Attempts left:", attempt)
+            elif number == medium:
                 print(f"Good job, {user}! You guessed my number!")
                 break
-    elif level == harder:
+            else:
+                attempt = 0
+                print("Sorry you lose")
+    elif level == "harder":
         print(f"Well, {user}, I am thinking of a number between 1 and 1000")
-        while number != harder:
+        while attempt > 0:
             number = int(input("Take a guess: "))
             if number > harder:
                 print("Your guess is too high")
+                attempt -= 1
+                print("Attempts left:", attempt)
             elif number < harder:
                 print("Your guess is too low")
-            else:
+                attempt -= 1
+                print("Attempts left:", attempt)
+            elif number == harder:
                 print(f"Good job, {user}! You guessed my number!")
                 break
+            else:
+                attempt = 0
+                print("Sorry you lose")
 
 
 
--- week4/learning-app.py
+++ week4/learning-app.py
@@ -12,27 +12,66 @@ def main():
 
         number1 = random.randint(1,20)
         number2 = random.randint(1,15)
-        guess = int()
+        guess = float()
         add = number1 + number2
+        multy = number1 * number2
+        sub = number1 - number2
+        div = round(number1 / number2,2)
+        operation = ("+","*","-","/")
+        op = random.choice(operation)
 
-        print(f"What is {number1} + {number2}?")
-        guess = input("Your answer: ")
+        print(f"What is {number1} {op} {number2}?")
+        guess = round(float(input("Your answer: ")),2)
 
         if guess == add:
             streak += 1
-            print("Correct!, next question")
             if streak == 1:
                 print("streak:", star)
+                print("Correct!, next question")
             elif streak == 2:
-                print("streak:", star, star)
+                print("streak:", star + star)
+                print("Correct!, next question")
             else:
-                print("Streak:",star,star,star)
-        elif guess != add:
-            print("Incorrect")
+                print("Streak:",star + star + star)
+                print("You are a genius!, see you next time")
+
+        elif guess == multy:
+            treak += 1
+            if streak == 1:
+                print("streak:", star)
+                print("Correct!, next question")
+            elif streak == 2:
+                print("streak:", star + star)
+                print("Correct!, next question")
+            else:
+                print("Streak:",star + star + star)
+                print("You are a genius!, see you next time")
+        elif guess == sub:
+            streak += 1
+            if streak == 1:
+                print("streak:", star)
+                print("Correct!, next question")
+            elif streak == 2:
+                print("streak:", star + star)
+                print("Correct!, next question")
+            else:
+                print("Streak:",star + star + star)
+                print("You are a genius!, see you next time")
+        elif guess == div:
+            streak += 1
+            if streak == 1:
+                print("streak:", star)
+                print("Correct!, next question")
+            elif streak == 2:
+                print("streak:", star + star)
+                print("Correct!, next question")
+            else:
+                print("Streak:",star + star + star)
+                print("You are a genius!, see you next time")
 
         else:
-            streak == 3
-            break
+            if streak == 3:
+                break
 
 
 if __name__=="__main__":
