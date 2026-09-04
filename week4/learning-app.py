89553ab16f690d6f401753b5f0e8ecefe6c4513e
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-04 20:58:58 +0000

    pam

--- week4/learning-app.py
+++ week4/learning-app.py
@@ -8,22 +8,31 @@ def main():
     streak = 0
     star = "⭐"
 
-
-    print(f"What is {number1} + {number2}?")
-    guess = input("Your answer: ")
-
     while streak != 3:
+
         number1 = random.randint(1,20)
         number2 = random.randint(1,15)
         guess = int()
         add = number1 + number2
-            if guess == add:
-                print("Correct!, next question")
+
+        print(f"What is {number1} + {number2}?")
+        guess = input("Your answer: ")
+
+        if guess == add:
+            streak += 1
+            print("Correct!, next question")
+            if streak == 1:
                 print("streak:", star)
+            elif streak == 2:
+                print("streak:", star, star)
             else:
-                print("Wrong")
-                streak +=1
-                print("streak:", star)
+                print("Streak:",star,star,star)
+        elif guess != add:
+            print("Incorrect")
+
+        else:
+            streak == 3
+            break
 
 
 if __name__=="__main__":
