3239f2d60e3203d7e32d6455ed20e55ea48805c5
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-18 20:21:29 +0000

    al profe no le gusta mi trabajo

---
+++ week5/for-intro.py
@@ -0,0 +1,10 @@
+def main():
+
+    doctrine = ["Faith in Jesus Christ", "Repentance", "Baptizm", "Receiving the gift of the Holy Ghost", "Endure"]
+    for number, principles in enumerate(doctrine, start = 1):
+        print(f"{number}.{principles}")
+
+
+if __name__=="__main__":
+    main()
+
