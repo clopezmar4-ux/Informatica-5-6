723a8947e91fc5b04e7622ada068d03da6a81bb7
clopezmar4-ux <clopezmar4@gmail.com>
2026-09-23 17:42:40 +0000

    es hora de aventura llama a tus amigos, vamos a tierras muy lejanas

---
+++ week5/tables.py
@@ -0,0 +1,28 @@
+def main():
+
+    number = [1,2,3,4,5,6,7,8,9,10]
+    while True:
+            user = input("Do you want a multiplication table? ").strip().lower()
+            if user == "no":
+                 print("Thanks for aswering")
+                 break
+            if user == "yes":
+                table = int(input("Enter a number (1-10): "))
+                if table in number:
+                    for num in range(len(number)):
+                        times = table * (num+1)
+                        print(f"{num+1} times {table} is {times}")
+                followup= input("Do you want another one? if not write exit: ").strip().lower()
+            if followup == "exit":
+                 print("Thanks for aswering")
+                 break
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
